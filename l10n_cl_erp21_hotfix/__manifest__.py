94b3340151f34cd0964082da6180615dfa68144c
rfloresl <r.flores.l@gmail.com>
2021-01-11 09:13:06 -0300

    Ordena template Informations

--- l10n_cl_erp21_hotfix/__manifest__.py
+++ l10n_cl_erp21_hotfix/__manifest__.py
@@ -2,7 +2,7 @@
 {
     'name': 'l10n_cl ErpSiglo21 Hotfix',
     'summary': 'Correcciones al modulo de facturacion electronica',
-    'version': '0.1',
+    'version': '0.2',
     'author': 'ERP Siglo 21',
     'category': 'Accounting/Localizations/Account Charts',
     'license': 'AGPL-3',
